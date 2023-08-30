228867d8a8bb7570b984e8829c99cef0b6793775
Kyrylo <fresh.js.ftd.@gmail.com>
2023-08-30 15:08:53 +0300

    Add vscode extension usage example

---
+++ scripts/CRUD.mongodb.js
@@ -0,0 +1,8 @@
+// коннекшн стринга
+// mongodb://127.0.0.1:PORT/DB-NAME
+// или
+// mongodb+srv://LOGIN:PASSWORD@cluster0.ozlvdnp.mongodb.net/DB
+db.users.insertOne({});
+
+// ctrl + alt + s - запуск выделенных строчек
+db.users.find();
