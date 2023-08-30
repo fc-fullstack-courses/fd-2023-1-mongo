60907a3c592365332dd5e77c45e8f05aed979464
Kyrylo <fresh.js.ftd.@gmail.com>
2023-08-30 15:21:10 +0300

    Add create examples

--- scripts/CRUD.mongodb.js
+++ scripts/CRUD.mongodb.js
@@ -6,3 +6,39 @@ db.users.insertOne({});
 
 // ctrl + alt + s - запуск выделенных строчек
 db.users.find();
+
+// CREATE
+// insertOne
+db.users.insertOne({
+  email: "test@test.test",
+  password: "12345sakdnfdsokf",
+  isMale: true,
+  address : {
+    countty: "UA",
+    city: "ZP",
+  },
+  phoneNumbers: [
+    '036547896',
+    '06547876546',
+    '1354897643433'
+  ]
+});
+
+// insertMany
+db.users.insertMany([
+  {
+    email: 'mail1@mail.com',
+  },
+  {
+    email: 'mail2@mail.com',
+  },
+  {
+    email: 'mail3@mail.com',
+  },
+  {
+    email: 'mail4@mail.com',
+  },
+  {
+    email: 'mail5@mail.com',
+  },
+]);
