acc97a373c6aeecd6082c5e27ff2c7f95ea436ae
Kyrylo <fresh.js.ftd.@gmail.com>
2023-09-01 14:57:35 +0300

    Add constants

---
+++ constants.js
@@ -0,0 +1,6 @@
+const CONSTANTS = {
+  PORT: process.env.PORT || 5000,
+  DB_URL: process.env.DB_URL || 'mongodb+srv://admin:admin@cluster0.ozlvdnp.mongodb.net/chatDB'
+}
+
+module.exports = CONSTANTS;
