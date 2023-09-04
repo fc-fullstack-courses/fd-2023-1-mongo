3bc5244daff28de9758ec354f28e48198e2600ab
Kyrylo <fresh.js.ftd.@gmail.com>
2023-09-04 17:04:29 +0300

    Add basic services example

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,17 +1,15 @@
 const createHttpError = require('http-errors');
+const MessageService = require('../services/message.service');
 const { Message, User } = require('../models');
 
 module.exports.createMessage = async (req, res, next) => {
   try {
     const { user, body } = req;
 
-    const message = await Message.create({
+    const message = await MessageService.createMessage({
       ...body,
-      user: user._id
+      user
     });
-
-    // дописать в массив messages у юзера айдишник нового сообщения
-    await user.updateOne({ $push: { messages: message._id } });
 
     res.status(201).send({ data: message });
   } catch (error) {
@@ -22,7 +20,7 @@ module.exports.createMessage = async (req, res, next) => {
 module.exports.getAllMessages = async (req, res, next) => {
   try {
 
-    const messages = await Message.find();
+    const messages = await MessageService.findMessages();
 
     res.send({ data: messages });
   } catch (error) {
@@ -34,17 +32,19 @@ module.exports.getUserMessages = async (req, res, next) => {
   try {
     const { user } = req;
 
-    const userMessages = await Message.find({
+    // const userMessages = await Message.find({
+    //   user: user._id
+    // }, '-__v')
+    //   .populate('user', 'firstName lastName email isOnline');
+    const userMessages2 = await MessageService.findMessages({
       user: user._id
-    }, '-__v')
-      // .populate('user', 'firstName lastName email isOnline');
-      .populate({
-        path: 'user',
-        select: 'firstName lastName email isOnline'
-      });
+    }, '-__v', {
+      path: 'user',
+      select: 'firstName lastName email isOnline'
+    });
 
 
-    res.send({ data: userMessages });
+    res.send({ data: userMessages2 });
   } catch (error) {
     next(error)
   }
---
+++ services/message.service.js
@@ -0,0 +1,33 @@
+const { Message } = require('../models');
+
+/**
+ * 
+ * @param {object} messageData 
+ * @param {object} messageData.user message author object
+ * @param {string} messageData.body message text
+ * @returns {object} new message
+ */
+module.exports.createMessage = async ({ user, body }) => {
+  const newMessage = await Message.create({
+    ...body,
+    user: user._id
+  });
+
+  // дописать в массив messages у юзера айдишник нового сообщения
+  await messageData.user.updateOne({ $push: { messages: newMessage._id } });
+
+  return newMessage;
+}
+
+module.exports.findMessages = async (filter, select, populate) => {
+
+  console.log(filter);
+  console.log(select);
+  console.log(populate);
+
+  const messages = await Message.find(filter, select).populate(populate || '');
+
+  console.log(messages);
+
+  return messages;
+}
