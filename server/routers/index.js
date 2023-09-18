c58a5c1bf151fe1fc1232e68c2568ee89fdf0ed0
Kyrylo <fresh.js.ftd.@gmail.com>
2023-09-18 16:30:47 +0300

    Add getAllMessages route

--- server/routers/index.js
+++ server/routers/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const authRouter = require('./authRouter');
 const userRouter = require('./userRouter');
+const messageController = require('../controllers/messages.controller');
 
 router.use('/auth', authRouter);
 router.use('/users', userRouter);
+router.get('/messages', messageController.getAllMessages);
 
 module.exports = router;
